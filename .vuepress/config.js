cae94781ab94ecd3c5e19ff522e4733e8b61e133
dgilleland <dgilleland@nait.ca>
2018-07-09 09:09:37 -0600

    Update w. build script et.al.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,8 +7,14 @@ module.exports = {
             md.use(require('markdown-it-imsize'))
             md.use(require('markdown-it-mermaid').default) // leave default options
             md.use(require('markdown-it-checkbox'))
+            md.use(require('markdown-it-kbd'))
             md.use(require('markdown-it-deflist'))
-        }
+            md.use(require('markdown-it-abbr'))
+            md.use(require('markdown-it-mark'))
+            md.use(require('markdown-it-footnote'))
+            md.use(require('markdown-it-sub'))
+            md.use(require('markdown-it-sup'))
+              }
     },
     themeConfig: {
         nav: [
