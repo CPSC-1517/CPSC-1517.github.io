639c9dc17fc023d0d71cca215cfe02dc90ce949c
Dan Gilleland <dgilleland@users.noreply.github.com>
2018-07-09 14:27:22 -0600

    Move Logs up after Home menu position

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -19,12 +19,12 @@ module.exports = {
     themeConfig: {
         nav: [
             { text: 'Home', link: '/about/' },
+            { text: 'LOGs', link: '/logs/' },
             { text: 'C-Sharp', link: '/csharp/' },
             { text: 'Html', link: '/html/' },
             { text: 'Razor', link: '/razor/' },
             { text: 'Client-Server', link: '/clientserver/' },
             { text: 'Webforms', link: '/webforms/' },
-            { text: 'LOGs', link: '/logs/' },
             { text: 'Issues/Bugs', link: 'https://github.com/CPSC-1517/CPSC-1517-StudentNotes/issues/new' },
             { text: 'Moodle', link: 'https://moodle.nait.ca' },
         ],
